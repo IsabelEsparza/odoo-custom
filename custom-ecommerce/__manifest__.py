e192bb8977aafbdfea9e2ed92f1ea38cba1e2bfb
IsabelEsparza <isa_tkm_ris@live.com.mx>
2019-10-13 16:51:50 -0500

    [ADD]Add new module

---
+++ custom-ecommerce/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2019 - Isabel Esparza
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+
+{
+    "name": "Email template",
+    "summary": "Email templates",
+    "version": "12.0.1.0.0",
+    "author": "Isabel Esparza, "
+              "Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/stock-logistics-workflow",
+    "category": "Templates",
+    "license": "AGPL-3",
+    "depends": [
+        "mail",
+    ],
+    "data": [
+        'data/template_welcome.xml',
+    ],
+    "installable": True,
+}
